bfd0f0bf2161366730700a1ff8ad7f3d777a77a1
nayeemdev <nayeem9812@gmail.com>
2021-05-25 15:53:38 +0600

    feat: QouteComponent added and chanegd app.js with some needed function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>RandomQoute</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import Quote from "./components/QuoteComponent";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: 'Life is movement-we breathe, we eat, we walk, we move!',
+      author: 'John Pierrakos',
+    };
+  }
+  randomQuote() {
+    //TODO: 
+  }
+
+  shuffleQuotes(array){
+    //TODO: 
+  }
+
+  handleClick = () => {
+    //TODO: 
+  };
+
+  randomColor() {
+    const color = `rgb(
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)})`;
+    return color;
+  }
+  
+  render() {
+    return (
+      <div>
+        <Quote
+          displayColor={this.randomColor}
+          handleClick={this.handleClick}
+          {...this.state}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
